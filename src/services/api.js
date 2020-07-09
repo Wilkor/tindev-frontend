2f854b0cc0ed69a4238ebe04652ca178edd40c24
Wilkor <34819624+Wilkor@users.noreply.github.com>
2020-07-09 14:42:21 -0300

    Update api.js
    alterando endereço do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'https://tindev-backend-wilkor.herokuapp.com'
+    baseURL:'https://tindev-wilkor-backend.herokuapp.com'
 })
 
 
